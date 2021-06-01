5c9a0f84e7c95ffe6df5294880dd0e5d6a378eec
Qyir <voyoaugi@gmail.com>
2021-06-01 22:57:32 +0800

    Update

--- src/settings.425ad.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Index.fire",orientation:"",jsList:[],bundleVers:{internal:"198ac",resources:"38f05",main:"49ef2"}};
---
+++ src/settings.f9bca.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Index.fire",orientation:"",jsList:[],bundleVers:{internal:"198ac",resources:"bb709",main:"18890"}};
