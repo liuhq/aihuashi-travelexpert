52162b6a4cc7e1fbf77597b6af35ed890394cbbd
Qyir <voyoaugi@gmail.com>
2021-06-01 12:02:39 +0800

    Update

---
+++ src/settings.53d6b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Menu.fire",orientation:"",jsList:[],bundleVers:{internal:"198ac",resources:"cf9fe",main:"c9893"}};
