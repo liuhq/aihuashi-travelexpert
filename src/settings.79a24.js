9b08afeb1bf68dcf2bb8c69de2e547e1ae890432
Qyir <voyoaugi@gmail.com>
2021-06-01 13:14:09 +0800

    Update

--- src/settings.79a24.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Menu.fire",orientation:"",jsList:[],bundleVers:{internal:"198ac",resources:"a694d",main:"5b2e5"}};
