56d4bacf3a694fd7108d0bc27e6bd6e124949f41
Qyir <voyoaugi@gmail.com>
2021-06-02 13:20:57 +0800

    Update

--- src/settings.bc1d6.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Menu.fire",orientation:"",jsList:[],bundleVers:{internal:"198ac",resources:"090a0",main:"e2c4f"}};
