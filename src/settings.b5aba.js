7a62f64cd57b475d4e897a536d2adde0c13f0a41
Qyir <voyoaugi@gmail.com>
2021-06-02 09:14:03 +0800

    Update

--- src/settings.b5aba.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Index.fire",orientation:"",jsList:[],bundleVers:{internal:"198ac",resources:"7550c",main:"6a489"}};
